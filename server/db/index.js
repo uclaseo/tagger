92345673f8247f9874d2ac535978dd3ac32b112d
inseok <inseok_ucla@gmail.com>
2017-06-09 11:19:03 -0700

    implements basic server and user information table

--- server/db/index.js
+++ server/db/index.js
@@ -1,3 +1,25 @@
 // set up a database connection with the sqlite3 ORM of your choice.
 // you'll need to refer to the docs for the exact set up.
 // checkout the README for some tips for how to clear out your database.
+const path = require('path');
+const knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: path.join(__dirname, 'notes.sqlite')
+  },
+  useNullAsDefault: true
+});
+const db = require('bookshelf')(knex);
+
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 255).unique();
+      user.string('password', 255);
+      user.timestamps();
+    }).then(function (table) {
+      console.log('User Information Table Created', table);
+    })
+  }
+})
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const app = express();
 const bodyParser = require('body-parser');
 
 // bring in any express middleware functions and other libraries you need
@@ -7,7 +8,7 @@ const bodyParser = require('body-parser');
 // this will force the db/index.js module to run, establishing a database connection.
 // you may or may not need to use the database connection in this index.js file.
 // if you need to use it, assign the return value of require('./db') to a variable.
-require('./db');
+const db = require('./db');
 
 // create an express instance 
 
@@ -19,3 +20,13 @@ require('./db');
 
 // listen on this port:
 const port = 5050;
+// app.set('view', __dirname + '/public');
+// app.set('view engine', 'ejs');
+
+app.listen(port, function() {
+  console.log('Server is running');
+})
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+})
